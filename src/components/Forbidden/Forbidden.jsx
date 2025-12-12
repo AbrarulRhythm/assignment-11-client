a43f0aee4fa86e715b2e19dd9e43ca76220eec54
Abrarul Rhythm <[....email>
2025-12-12 23:30:50 +0600

    added forbidden page

--- src/components/Forbidden/Forbidden.jsx
+++ src/components/Forbidden/Forbidden.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import forbiddenAnimation from '../../assets/animations/forbidden403.json';
+import Lottie from 'lottie-react';
+import { Link } from 'react-router';
 
 const Forbidden = () => {
     return (
-        <div>
-            Forbidden
+        <div className='py-10 lg:py-20 mx-auto text-center'>
+            <div className='w-[190px] md:w-[200px] lg:w-[250px] mx-auto mb-6'>
+                <Lottie
+                    animationData={forbiddenAnimation}
+                    loop={false}
+                ></Lottie>
+            </div>
+            <h1 className='text-dark-09 font-semibold mb-4 text-center text-2xl md:text-3xl lg:text-4xl'>You're not permitted to see this.</h1>
+            <Link to='/dashboard/overview' className='btn btn-info'>Return Home</Link>
         </div>
     );
 };
